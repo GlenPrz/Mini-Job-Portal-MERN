d590c74e004dede6dffbd45bffe421ab1bd7fd8a
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2025-09-19 23:15:20 +0800

    modified:   server/models/Jobs.js

--- server/models/Jobs.js
+++ server/models/Jobs.js
@@ -1,19 +1,13 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
+const jobSchema = new mongoose.Schema ({
     title: {
         type: String,
         required: true,
-        minlength: 3,
-        maxlength: 100,
-        trim: true,
     },
     description: {
         type: String,
         required: true,
-        minlength: 10,
-        maxlength: 1000,
-        trim: true,
     },
     dateCreated: {
         type: Date,
@@ -22,10 +16,8 @@ const jobSchema = new mongoose.Schema({
     department: {
         type: String,
         required: true,
-        enum: ["Engineering", "Marketing", "Sales", "HR", "Finance"], // example departments
-        trim: true,
     },
-});
+})
 
-const JobModel = mongoose.model("Jobs", jobSchema);
+const JobModel = mongoose.model("Jobs", jobSchema)
 export default JobModel;
