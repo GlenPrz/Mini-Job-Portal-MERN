240db939f9ff55351218d177b7ba1278a2a256a0
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2025-09-24 00:24:58 +0800

    Start working on frontend 	modified:   client/README.md 	modified:   client/index.html 	modified:   client/package-lock.json 	modified:   client/package.json 	deleted:    client/src/App.css 	modified:   client/src/App.jsx 	modified:   client/src/pages/Login.jsx 	modified:   client/vite.config.js

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,27 @@ import '../styles/Login.css';
 
 const Login = () => {
   return (
-    <div></div>
+    <div className='main-container'>
+        <div className='form-card'>
+            <h1>Login</h1>
+            <form className=''>
+                <div className='row'>
+                    <input type='text' className='username' id='username' autoComplete='off' /> 
+                    <label for='username' className='un_label'>Username</label>
+                </div>
+                <div className='row'>
+                    <input type='password' className='pass' id='pass' autoComplete='off' /> 
+                    <label for='pass' className='pw_label'>password</label>
+                </div>
+                <div className='row'>
+                    <p> Don't have an account yet?<a href='link'> Sign up here!</a></p>
+                </div>
+            </form>
+
+            
+        </div>
+        
+    </div>
   )
 }
 
