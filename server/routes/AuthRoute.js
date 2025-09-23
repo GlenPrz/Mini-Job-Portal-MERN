53a48fcc8c594bb167761079aeba5bdab59839f0
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2025-09-23 23:19:33 +0800

    modified:   server/routes/AuthRoute.js

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -3,6 +3,6 @@ import { Signup } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
-router.post("/signup", Signup);
+router.post("/users", Signup);
 
 export default router;
