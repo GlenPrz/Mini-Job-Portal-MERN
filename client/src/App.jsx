b3e2b8e2ed959a363b66f35e267a937d111ddf30
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2025-09-29 23:10:14 +0800

    Added sidebar... W I P ... 	modified:   client/src/App.jsx 	new file:   client/src/components/Sidebar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/admin/Login'
-
+import { SidebarNavigationSlimDemo } from './components/Sidebar';
 function App() {
 
   return (
     <div>
       <BrowserRouter>
+      <SidebarNavigationSlimDemo/>
         <Routes>
           <Route path='/login' element={<Login />}></Route>
           {/* <Route path='/' element={< />}></Route>
