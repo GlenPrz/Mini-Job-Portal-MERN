9d2282541cc4dc29346f8b5ae057b6efa0f8dbad
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2025-10-01 23:40:08 +0800

    PROBLEMS 	new file:   client/.flowbite-react/.gitignore 	new file:   client/.flowbite-react/config.json 	new file:   client/.flowbite-react/init.tsx 	new file:   client/.vscode/extensions.json 	modified:   client/src/App.jsx 	modified:   client/src/components/Sidebar.jsx 	modified:   client/vite.config.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/admin/Login'
-import { SidebarNavigationSlimDemo } from './components/Sidebar';
+import SidebarNavigationSlimDemo from './components/Sidebar';
 function App() {
 
   return (
     <div>
       <BrowserRouter>
-            {/* <SidebarNavigationSlimDemo/> */}
 
         <Routes>
+                      <SidebarNavigationSlimDemo/>
+
           <Route path='/login' element={<Login />}></Route>
           {/* <Route path='/' element={< />}></Route>
           <Route path='/' element={< />}></Route>
